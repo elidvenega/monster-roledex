ea0762e61aed03c6c6f43b13682bf9a9a43c2d60
elidvenega <venegaelid@gmail.com>
2022-08-23 19:38:37 -0700

    Updated

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import { Component } from "react";
 
 import logo from "./logo.svg";
 import "./App.css";
-import { render } from "@testing-library/react";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: { firstName: "Eli", lastName: "Venega" },
+      company: "ZTM",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
+            {this.state.company}
           </p>
+          <button
+            onClick={() => {
+              // setState creates another object
+              this.setState({
+                name: { firstName: "Andrei", lastName: "Neaogie" },
+              });
+            }}
+          >
+            Click
+          </button>
           <a
             className="App-link"
             href="https://reactjs.org"
