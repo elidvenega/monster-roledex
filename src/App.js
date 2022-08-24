143fd66f06cec4c57b4539848fa0464d31044447
elidvenega <venegaelid@gmail.com>
2022-08-24 06:56:21 -0700

    Worked on React

--- src/App.js
+++ src/App.js
@@ -7,28 +7,35 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-        },
-        {
-          name: "Frank",
-        },
-        {
-          name: "Jacky",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
-      {
-        this.state.monsters.map((monster) => {
-           return <h1>{monster.name}</h1>
-        })
-      }
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1> {monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
